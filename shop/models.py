06cdfe85123f36d28ab5ddbfba935cde2e60fdbe
Simon Atiegar <atiegardelali@gmail.com>
2023-11-27 20:22:06 +0000

    create item model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField(default=0.00)
+    description = models.CharField(max_length=200)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView 
+from .models import Item
 
 # Create your views here.
 
-class Home(TemplateView):
+class Home(ListView):
+    model = Item
     template_name = 'shop/home.html'
+    context_object_name = 'items'
 
