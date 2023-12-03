c62a996c351bb8e655b640aa40f218968c245ac0
Simon Atiegar <atiegardelali@gmail.com>
2023-12-03 23:57:17 +0000

    detail

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('product/<slug:slug>/', views.ProductDetail.as_view(), name='product-detail'),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -8,4 +8,12 @@ class Home(ListView):
     model = Item
     template_name = 'shop/home.html'
     context_object_name = 'items'
+    paginate_by = 4
+
+class ProductDetail(DetailView):
+    model = Item
+    template_name = 'shop/product-detail.html'
+    context_object_name = 'item'
+
+
 
