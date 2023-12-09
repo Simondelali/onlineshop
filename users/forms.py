8d10550c25d64cb58348748cafc4ec4e327dc9a1
Simon Atiegar <atiegardelali@gmail.com>
2023-12-09 23:59:17 +0000

    fix

---
+++ users/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+# from .models import Profile
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name','email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name']
